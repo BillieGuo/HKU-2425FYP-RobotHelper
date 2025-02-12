cc5051cf7ab78664b5db07b3c1079751ad69ef86
Carbonadooo <zshaoyu@connect.hku.hk>
2025-02-12 12:04:33 +0800

    Complete the realsense_ros_test node, now the aligned and synced RGBD image savable.

--- grasp/gdino_sam/gdino_sam/realsense_ros_test.py
+++ grasp/gdino_sam/gdino_sam/realsense_ros_test.py
@@ -6,7 +6,7 @@ import numpy as np
 import rclpy
 from rclpy.node import Node
 from std_srvs.srv import Trigger
-from sensor_msgs.msg import Image as ROSImage
+from realsense2_camera_msgs.msg import RGBD  # Import the RGBD message type
 from std_msgs.msg import String
 from cv_bridge import CvBridge
 import threading
@@ -14,7 +14,14 @@ import readchar  # Replaced keyboard with readchar
 
 os.environ["TORCH_CUDA_VERBOSE"] = "1"
 
-script_dir = os.path.dirname(os.path.abspath(__file__))
+save_dir = "/home/intern/fyp_ws/src/HKU-2425FYP-RobotHelper/grasp/gdino_sam/assets/realsense_ros_test"
+os.makedirs(save_dir, exist_ok=True)
+# RGBD Message
+# std_msgs/Header header
+# sensor_msgs/CameraInfo rgb_camera_info
+# sensor_msgs/CameraInfo depth_camera_info
+# sensor_msgs/Image rgb
+# sensor_msgs/Image depth
 
 
 class realsense_test_node(Node):
@@ -23,7 +30,7 @@ class realsense_test_node(Node):
 
         # Subscribe both color and depth images
         rgbd_topic = f"/{camera_namespace}/{camera_name}/rgbd"
-        self.rgbd_sub = self.create_subscription(ROSImage, rgbd_topic, self.rgbd_callback, 10)
+        self.rgbd_sub = self.create_subscription(RGBD, rgbd_topic, self.rgbd_callback, 10)
 
         self.bridge = CvBridge()
         self.latest_color_image = None
@@ -33,7 +40,9 @@ class realsense_test_node(Node):
         # Start keyboard listener thread
         threading.Thread(target=self.listen_keyboard, daemon=True).start()
 
-        self.get_logger().info("Realsense test node has been started.")
+        self.get_logger().info(
+            f"Realsense test node has been started.\nNamespace/Name: {camera_namespace}/{camera_name}\nPress q to quit, s to save images."
+        )
 
     def listen_keyboard(self):
         while rclpy.ok():
@@ -41,15 +50,18 @@ class realsense_test_node(Node):
             if char == "s":
                 self.save_image = True
                 self.get_logger().info("Save command received.")
+            elif char == "q":
+                self.get_logger().info("Quit command received.")
+                rclpy.shutdown()
 
     def rgbd_callback(self, msg):
         self.latest_color_image = self.bridge.imgmsg_to_cv2(msg.rgb, "bgr8")
         self.latest_depth_image = self.bridge.imgmsg_to_cv2(msg.depth, "16UC1")
-        print(f"Color image shape: {self.latest_color_image.shape}")
+        # print(f"Color image shape: {self.latest_color_image.shape}")
         # Image saving functionality triggered by key press
         if self.save_image and self.latest_color_image is not None and self.latest_depth_image is not None:
-            color_path = os.path.join(script_dir, "saved_color.png")
-            depth_path = os.path.join(script_dir, "saved_depth.png")
+            color_path = os.path.join(save_dir, "saved_color.png")
+            depth_path = os.path.join(save_dir, "saved_depth.png")
             cv2.imwrite(color_path, self.latest_color_image)
             cv2.imwrite(depth_path, self.latest_depth_image)
             self.get_logger().info(f"Images saved to {color_path} and {depth_path}.")
@@ -62,7 +74,7 @@ def main(args=None):
     try:
         rclpy.spin(node)
     except KeyboardInterrupt:
-        node.get_logger().info("Program interrupted. Exiting...")
+        pass
     finally:
         node.destroy_node()
         rclpy.shutdown()
